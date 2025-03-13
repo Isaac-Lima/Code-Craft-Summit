a715f48c94c9c7341191ccce272c56d2c17afd88
isaac-lima <isaac.lima25@aluno.ce.gov.br>
2025-03-12 22:46:28 -0300

    :sparkles: feat: integrando o back-end e gerando cliente HTTP

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -1,13 +1,19 @@
 import Image from 'next/image'
 
-import logo from '../../assets/Logo.svg'
+import logo from '../../../assets/Logo.svg'
 import { Ranking } from './ranking'
 import { Stats } from './stats'
 import { InviteLinkInput } from './invite-link-input'
 
+interface InvitePageProps{
+    params: Promise<{
+        subscriberId: string
+    }>
+}
 
-export default function InvitePage(){
-    const inviteLink = 'devstage.com/codecraft-summit-2025/1289'
+export default async function InvitePage(props: InvitePageProps){
+    const {subscriberId} = await props.params
+    const inviteLink = `http://localhost:3333/invites/${subscriberId}`
 
     return(
         <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
@@ -27,7 +33,7 @@ export default function InvitePage(){
 
                     <InviteLinkInput inviteLink={inviteLink} />
 
-                    <Stats/>
+                    <Stats subscriberId={subscriberId}/>
                 </div>
             </div>
 
--- src/app/invite/[subscriberId]/stats.tsx
+++ src/app/invite/[subscriberId]/stats.tsx
@@ -1,17 +1,27 @@
+import { getSubscriberInviteClicks, getSubscriberRankingPosition } from '@/http/api'
 import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react'
 
-export function Stats(){
+interface StatsProps{
+    subscriberId: string
+}
+
+export async function Stats({ subscriberId }: StatsProps){
+
+    const { count: acessCount } = await getSubscriberInviteClicks(subscriberId)
+    const { count: inviteCount } = await getSubscriberInviteClicks(subscriberId)
+    const { position: rankingPositition } = await getSubscriberRankingPosition(subscriberId)
+
     return(
         <div className='grid gap-3 md:grid-cols-3'>
                         <div className='relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl'>
-                            <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>1042</span>
+                            <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>{acessCount}</span>
                             <span className='text-sm text-gray-300 leading-none text-center'>Acessos ao link</span>
 
                             <MousePointerClick className='size-5 text-purple absolute top-3 left-3'/>
                         </div>
 
                         <div className='relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl'>
-                            <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>1042</span>
+                            <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>{inviteCount}</span>
                             <span className='text-sm text-gray-300 leading-none text-center'>Inscrições feitas</span>
 
                             <BadgeCheck className='size-5 text-purple absolute top-3 left-3'/>
@@ -19,7 +29,7 @@ export function Stats(){
                         </div>
 
                         <div className='relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl'>
-                            <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>3°</span>
+                            <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>{rankingPositition ? `${rankingPositition}°` : '-'}</span>
                             <span className='text-sm text-gray-300 leading-none text-center'>Posição no ranking</span>
 
                             <Medal className='size-5 text-purple absolute top-3 left-3'/>
